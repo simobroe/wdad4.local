f75cb87d9eae9e9638a1cce3d501c769369aafc6
Julien Cambier <julien@juliens-mbp.home>
2016-12-08 19:09:52 +0100

    begin frontoffice, ionic

---
+++ dotnet/src/BnbGo.AJS/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Home } from '../pages/home/home';
+import { Page2 } from '../pages/page2/page2';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    Home,
+    Page2
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    Home,
+    Page2
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
